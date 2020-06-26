0b2744d804eacb3ef025a3ba57296a3327671825
Daniel Finimundi <daniel@finimundi.com>
2020-06-26 16:15:27 -0300

    Remove fpms networkinfo from cron job
    This script is started from fpms script now instead of cron job.
    
    Also cleaning up other unneeded files from fpms package.
    
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- packages/extras-buildpkgs/fpms/debian/fpms.cron.d
+++
@@ -1,5 +0,0 @@
-#
-# Regular cron jobs for the fpms package
-# See dh_installcron(1) and crontab(5).
-#
-@reboot     root    /usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/networkinfocron.sh
