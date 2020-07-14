c50fd4371faf1d30c415c2683eb129bf8bdb3d4c
Daniel Finimundi <daniel@finimundi.com>
2020-07-13 22:54:56 -0300

    Reenable NetworkManager and make it manage only eth
    NetworkManager is only a problem for wireless interfaces, let's keep it
    enabled but restrict it to manage only ethernet interfaces.
    
    Signed-off-by: Daniel Finimundi <daniel@finimundi.com>

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -162,11 +162,11 @@ SetupOtherServices() {
 	display_alert "Setup service" "iperf2" "info"
 	copy_overlay /lib/systemd/system/iperf2.service -o root -g root -m 644
 
-	display_alert "Disable service" "NetworkManager" "info"
-	systemctl disable NetworkManager.service
-	systemctl disable NetworkManager-wait-online.service
-	systemctl disable NetworkManager-dispatcher.service
-	systemctl disable network-manager.service
+	display_alert "Configure service" "NetworkManager" "info"
+	cat <<-EOF >> /etc/NetworkManager/NetworkManager.conf
+[keyfile]
+unmanaged-devices=*,except:type:ethernet
+EOF
 }
 
 SetupOtherConfigFiles() {
