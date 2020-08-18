9ddce1ec8bf52a7e7828608e80c85cb0bc30b194
Josh Schmelzle <13954434+joshschmelzle@users.noreply.github.com>
2020-08-17 20:35:38 -0400

    copy profiler config to /etc/profiler2

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -79,6 +79,7 @@ InstallProfilerPipx() {
 	
 	pipx install git+https://github.com/joshschmelzle/profiler2.git@main#egg=profiler2
 	copy_overlay /lib/systemd/system/profiler.service -o root -g root -m 644
+	copy_overlay /etc/profiler2/config.ini -o root -g root -m 644
 }
 
 SetupCockpit() {
