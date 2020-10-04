f73215f79446bfc5e892863b30ac651fdc2efafd
Jiri Brejcha <jirka@jiribrejcha.net>
2020-10-04 22:49:47 +0000

    Add networkinfo aliases

--- userpatches/customize-image.sh
+++ userpatches/customize-image.sh
@@ -265,9 +265,18 @@ SetupOtherConfigFiles() {
 
 	display_alert "Change default systemd boot target" "multi-user.target" "info"
 	systemctl set-default multi-user.target
-	
+
 	display_alert "Copy config file" "RF Central Regulatory Domain Agent" "info"
-	copy_overlay /etc/default/crda -o root -g root -m 644	
+	copy_overlay /etc/default/crda -o root -g root -m 644
+
+	cat <<-EOF >> /home/wlanpi/.bashrc
+alias reachability='sudo /usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/reachability.sh'
+alias publicip='/usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/publicip.sh'
+alias watchinternet='/usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/watchinternet.sh'
+alias telegrambot='/usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/telegrambot.sh'
+alias ipconfig='sudo /usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/ipconfig.sh'
+alias portblinker='/usr/share/fpms/BakeBit/Software/Python/scripts/networkinfo/portblinker.sh'
+EOF
 }
 
 InstallMongoDB() {
